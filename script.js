82ee91a467392dea803fe56653b267d5e7841662
aswanthnarayan <aswanthnarayanan@gmail.com>
2023-11-16 01:45:59 +0530

    completed the styling , rearranged the js code for better reading ,add new function for  1st loading of app

--- script.js
+++ script.js
@@ -4,23 +4,34 @@
     const contentDiv=document.querySelector(".content");
 
 
-    
+    function onloadFetch(){
+        const city = 'kannur';
+        fetch(`
+        https://api.weatherapi.com/v1/forecast.json?key=40df1897895146cbbe4211346231211&q=${city}&days=3&aqi=yes&alerts=yes`)
+            .then(response => response.json())
+            .then(data => {
+                    WeatherDataCreation(data);
+                    forecastDataCreation(data);
+                    otherDataCreation(data);
+            })
+            .catch(error => {
+                // console.error('Error fetching data:', error);
+                // temperature.innerText="No Data Found"
+                // cityName.innerHTML="";
+                // weatherInfo.innerText="";  
+                // temperature.style.fontSize="1.5rem";
+            });
+    }
+
 
 function getWeather() {
-
-
-
     const existingForecast=document.querySelector('.forecast');
     const existingDetails = document.querySelector('.main-details')
     const existingOtherDetails = document.querySelector('.other-details')
 
-        if(existingForecast){
+        if(existingForecast||existingDetails||existingOtherDetails){
             existingForecast.remove(); 
-        }
-        if(existingDetails){
             existingDetails.remove();
-        }
-        if(existingOtherDetails){
             existingOtherDetails.remove();
         }
     const city = cityInput.value;
@@ -28,86 +39,9 @@ function getWeather() {
     https://api.weatherapi.com/v1/forecast.json?key=40df1897895146cbbe4211346231211&q=${city}&days=3&aqi=yes&alerts=yes`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-                let WeatherCondition= data.current.condition.text;
-                console.log(WeatherCondition);
-                const temp = data.current.temp_c;
-                let imgSrc=`https:${data.current.condition.icon}`;
-
-                const mainDetails = document.createElement('div');
-                mainDetails.className='main-details';
-                mainDetails.innerHTML=`
-                   <p id="city-name">${data.location.name}</p>
-                   <div class="data">
-                      <h1 id="temperature">${Math.round(temp)}&deg</h1>
-                    <div class="condition">
-                      <p id="weather-info">${data.current.condition.text}</p>
-                    <img src=${imgSrc} alt=${data.current.condition.text}>
-                    </div>
-                `
-                contentDiv.appendChild(mainDetails)
-                backgroundGenerator(WeatherCondition);
-
-            let forecastContainer=document.createElement('div');
-                forecastContainer.className='forecast-container'
-                let forecast=document.createElement('div');
-                forecast.className='forecast'
-                forecast.innerHTML=`<p>3 Days Forecast</p>`
-                data.forecast.forecastday.forEach(days => {
-                    let forecastDetails=document.createElement('div');
-                    forecastDetails.className="forecast-main";
-                    forecastDetails.innerHTML=`
-                      <p>${days.date}</p> 
-                        <div class="forecast-data">
-                            <p>${days.day.condition.text}</p>
-                            <img src="${days.day.condition.icon}" alt="${days.day.condition.text}">
-                        </div>
-                        <div class="temp">
-                            <p>Min: ${days.day.mintemp_c}</p>
-                            <p>Max: ${days.day.maxtemp_c}</p>
-                        </div>
-                        `
-                            forecastContainer.appendChild(forecastDetails)
-                            forecast.appendChild(forecastContainer)
-                })    
-                
-                contentDiv.appendChild(forecast) ;
-
-
-                let otherDetailes=document.createElement('div');
-                otherDetailes.className='other-details'
-                otherDetailes.innerHTML=`
-                    <div class="humidity">
-                
-                        <div class="feels-like">
-                            <p>Feelslike</p>
-                            <p>${data.current.feelslike_c}</p>
-                        </div>
-                        <div class="humidity-data">
-                            <p>Humidity</p>
-                            <p>${data.current.humidity}</p>
-                        </div>
-                    </div>
-                    <div class="wind">
-                        <div class="wind-kph">
-                        <p>${data.current.wind_kph}</p>
-                            <div class="measurement">
-                                <p>KPH</p>
-                                <p>Wind</p>
-                            </div>
-                        </div>
-                        <div class="wind-gust">
-                        <p>${data.current.gust_kph}</p>
-                            <div class="measurement">
-                                <p>KPH</p>
-                                <p>Guests</p>
-                            </div>
-                        </div>
-                    </div>
-                `
-                contentDiv.appendChild(otherDetailes) ;
-                   
-    
+                WeatherDataCreation(data);
+                forecastDataCreation(data);
+                otherDataCreation(data);
         })
         .catch(error => {
             // console.error('Error fetching data:', error);
@@ -119,12 +53,105 @@ function getWeather() {
 }
 
 
+window.addEventListener('DOMContentLoaded',onloadFetch)
 findButton.addEventListener('click',getWeather);
+
+
+function WeatherDataCreation(data){
+    let WeatherCondition= data.current.condition.text;
+    console.log(WeatherCondition);
+    const temp = data.current.temp_c;
+    let imgSrc=`https:${data.current.condition.icon}`;
+    const mainDetails = document.createElement('div');
+    mainDetails.className='main-details';
+    mainDetails.innerHTML=`
+       <p id="city-name">${data.location.name}</p>
+       <div class="data">
+          <h1 id="temperature">${Math.round(temp)}&deg</h1>
+        <div class="condition">
+          <p id="weather-info">${data.current.condition.text}</p>
+        <img src=${imgSrc} alt=${data.current.condition.text}>
+        </div>
+    `
+    contentDiv.appendChild(mainDetails)
+    backgroundGenerator(WeatherCondition);
+}
+
+
+function forecastDataCreation(data){
+    let forecastContainer=document.createElement('div');
+                forecastContainer.className='forecast-container'
+                let forecast=document.createElement('div');
+                forecast.className='forecast'
+
+                forecast.innerHTML=`<p>3 Days Forecast</p>`
+                data.forecast.forecastday.forEach(days => {
+                    let forecastDetails=document.createElement('div');
+                    forecastDetails.className="forecast-main";
+                    let forecastDate=dateConverter(days.date)
+                    forecastDetails.innerHTML=`
+                      <p class="date">${forecastDate}</p> 
+                        <div class="forecast-data">
+                            <p>${days.day.condition.text}</p>
+                            <img src="${days.day.condition.icon}" alt="${days.day.condition.text}">
+                        </div>
+                        <div class="temp">
+                            <p>Min: ${Math.round(days.day.mintemp_c)}&degc</p>
+                            <p>Max: ${Math.round(days.day.maxtemp_c)}&degc</p>
+                        </div>
+                        `
+                            forecastContainer.appendChild(forecastDetails)
+                            forecast.appendChild(forecastContainer)
+                })    
+                contentDiv.appendChild(forecast) ;
+}
+
+
+
+        function otherDataCreation(data){
+
+            let otherDetailes=document.createElement('div');
+            otherDetailes.className='other-details'
+            otherDetailes.innerHTML=`
+                <div class="humidity">
+            
+                    <div class="feels-like">
+                        <p>Feelslike</p>
+                        <p>${data.current.feelslike_c}&degc</p>
+                    </div>
+                    <div class="humidity-data">
+                        <p>Humidity</p>
+                        <p>${data.current.humidity}</p>
+                    </div>
+                </div>
+                <div class="wind">
+                    <div class="wind-kph">
+                    <p>${Math.round(data.current.wind_kph)}</p>
+                        <div class="measurement">
+                            <p>KPH</p>
+                            <p>Wind</p>
+                        </div>
+                    </div>
+                    <div class="wind-gust">
+                    <p>${Math.round(data.current.gust_kph)}</p>
+                        <div class="measurement">
+                            <p>KPH</p>
+                            <p>Guests</p>
+                        </div>
+                    </div>
+                </div>
+            `
+            contentDiv.appendChild(otherDetailes) ;
+               
+        }
+
+
 
 
 
   function backgroundGenerator(apiKey){
     let bgImg=contentDiv;
+    bgImg.style.backgroundSize = 'cover';
     switch (apiKey) {
         case "Clear":
             bgImg.style.backgroundImage = "url('/Images/Clear.png')";
@@ -134,11 +161,15 @@ findButton.addEventListener('click',getWeather);
                 break;
         case "Cloudy":
                 bgImg.style.backgroundImage = "url('/Images/Cloudy.png')";
+                
                 break;
         case "Overcast":
                 bgImg.style.backgroundImage = "url('/Images/Cloudy.png')";
                 break;
-        case "Rain":
+        case "Rain"||"Moderate or heavy rain shower":
+                bgImg.style.backgroundImage = "url('/Images/Rainy.png')";
+                break;
+        case "Moderate or heavy rain shower":
                 bgImg.style.backgroundImage = "url('/Images/Rainy.png')";
                 break;
         case "Snow":
@@ -173,4 +204,12 @@ findButton.addEventListener('click',getWeather);
                 break;
 
     }
+  }
+
+  // function for correcting the date format
+  function dateConverter(date){
+    //date='2023-05-10';
+    let newFormat=date.split('-');
+    let newDate=newFormat[2]+'/'+newFormat[1]+'/'+newFormat[0];
+    return newDate;
   }
